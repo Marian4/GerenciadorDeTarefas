cc2338b9ba3d7c182124f96b98b5775d2c828b97
Mariana Moraes <Mraes.Mariana@gmail.com>
2020-05-30 10:32:12 -0300

    b

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(morgan("dev"))
+app.use(cors())
 
 app.get('/', (req,res) =>{
     res.send("Olá");
